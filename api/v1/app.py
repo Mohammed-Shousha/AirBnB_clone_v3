f2379ecc556d11c990ccbb4104943b551bac161f
Mohammed Shousha <mshousha29@gmail.com>
2024-01-05 18:22:32 +0200

    Status of your API

---
+++ api/v1/app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+starts a Flask API
+"""
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+import os
+
+
+app = Flask(__name__)
+
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """executes after each request"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(
+        host=os.getenv("HBNB_API_HOST", "0.0.0.0"),
+        port=os.getenv("HBNB_API_PORT", "5000"),
+        threaded=True
+    )
